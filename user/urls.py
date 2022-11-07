36637ebf20e2a13fabf5e2ce3d8a7caa25942e0b
Toutchy kipik <toutchy.kipik@pm.me>
2022-11-07 21:08:48 +0100

    test

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,7 @@
-from django.conf.urls.static import static
-from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.contrib.auth.views import LogoutView
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
-from safepass import settings
+
 from user.forms import LoginForm
 from user.views import RegisterView, view_profile, profile, CustomLoginView, ResetPasswordView, ChangePasswordView
 
